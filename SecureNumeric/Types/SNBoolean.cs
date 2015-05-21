8d578416b3688fa86c5f98e1df4dc4fb5b76f20a
Wu Yuntao <wyt.brandon@gmail.com>
2015-05-21 22:41:28 +0800

    1, Add support for bool

---
+++ SecureNumeric/Types/SNBoolean.cs
@@ -0,0 +1,173 @@
+﻿using SecureNumeric.Encryptors;
+using System;
+
+namespace SecureNumeric.Types
+{
+	public struct SNBoolean : IComparable, IComparable<bool>, IComparable<SNBoolean>, IEquatable<bool>, IEquatable<SNBoolean>, IConvertible
+	{
+		SNSByte encryptedValue;
+
+		public SNBoolean(bool value)
+		{
+			encryptedValue = new SNSByte( (sbyte)( value ? 1 : -1 ) );
+		}
+
+		public bool Value
+		{
+			get { return encryptedValue.Value > 0; }
+			set { encryptedValue.Value = (sbyte)( value ? 1 : -1 ); }
+		}
+
+		#region IComparable
+
+		public int CompareTo(object obj)
+		{
+			if( obj is bool )
+				return CompareTo( (bool)obj );
+
+			if( obj is SNBoolean )
+				return CompareTo( (SNBoolean)obj );
+
+			throw new ArgumentException( "value must be a bool or SNBoolean" );
+		}
+
+		public int CompareTo(bool value)
+		{
+			return Value.CompareTo( value );
+		}
+
+		public int CompareTo(SNBoolean value)
+		{
+			return Value.CompareTo( value.Value );
+		}
+
+		#endregion
+
+		#region IFormattable
+
+		public override string ToString()
+		{
+			return Value.ToString();
+		}
+
+		public string ToString(IFormatProvider provider)
+		{
+			return Value.ToString( provider );
+		}
+
+		#endregion
+
+		#region IEquatable
+
+		public override bool Equals(object obj)
+		{
+			return Value.Equals( obj );
+		}
+
+		public override int GetHashCode()
+		{
+			return Value.GetHashCode();
+		}
+
+		public bool Equals(bool value)
+		{
+			return Value.Equals( value );
+		}
+
+		public bool Equals(SNBoolean value)
+		{
+			return Value.Equals( value.Value );
+		}
+
+		#endregion
+
+		#region IConvertable
+
+		public TypeCode GetTypeCode()
+		{
+			return Value.GetTypeCode();
+		}
+
+		object IConvertible.ToType(Type conversionType, IFormatProvider provider)
+		{
+			return ( (IConvertible)Value ).ToType( conversionType, provider );
+		}
+
+		bool IConvertible.ToBoolean(IFormatProvider provider)
+		{
+			return ( (IConvertible)Value ).ToBoolean( provider );
+		}
+
+		sbyte IConvertible.ToSByte(IFormatProvider provider)
+		{
+			return ( (IConvertible)Value ).ToSByte( provider );
+		}
+
+		byte IConvertible.ToByte(IFormatProvider provider)
+		{
+			return ( (IConvertible)Value ).ToByte( provider );
+		}
+
+		short IConvertible.ToInt16(IFormatProvider provider)
+		{
+			return ( (IConvertible)Value ).ToInt16( provider );
+		}
+
+		ushort IConvertible.ToUInt16(IFormatProvider provider)
+		{
+			return ( (IConvertible)Value ).ToUInt16( provider );
+		}
+
+		int IConvertible.ToInt32(IFormatProvider provider)
+		{
+			return ( (IConvertible)Value ).ToInt32( provider );
+		}
+
+		uint IConvertible.ToUInt32(IFormatProvider provider)
+		{
+			return ( (IConvertible)Value ).ToUInt32( provider );
+		}
+
+		long IConvertible.ToInt64(IFormatProvider provider)
+		{
+			return ( (IConvertible)Value ).ToInt64( provider );
+		}
+
+		ulong IConvertible.ToUInt64(IFormatProvider provider)
+		{
+			return ( (IConvertible)Value ).ToUInt64( provider );
+		}
+
+		float IConvertible.ToSingle(IFormatProvider provider)
+		{
+			return ( (IConvertible)Value ).ToSingle( provider );
+		}
+
+		double IConvertible.ToDouble(IFormatProvider provider)
+		{
+			return ( (IConvertible)Value ).ToDouble( provider );
+		}
+
+		decimal IConvertible.ToDecimal(IFormatProvider provider)
+		{
+			return ( (IConvertible)Value ).ToDecimal( provider );
+		}
+
+		char IConvertible.ToChar(IFormatProvider provider)
+		{
+			return ( (IConvertible)Value ).ToChar( provider );
+		}
+
+		string IConvertible.ToString(IFormatProvider provider)
+		{
+			return ( (IConvertible)Value ).ToString( provider );
+		}
+
+		DateTime IConvertible.ToDateTime(IFormatProvider provider)
+		{
+			return ( (IConvertible)Value ).ToDateTime( provider );
+		}
+
+		#endregion
+	}
+}
---
+++ SecureNumericTests/Tests/BooleanTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using SecureNumeric.Types;
+using System;
+
+namespace SecureNumeric.Tests
+{
+	public class BooleanTests
+	{
+		Random random = new Random();
+
+		[Test]
+		public void TestEquatable()
+		{
+			var boolVal = RandomBoolValue;
+			var snBooleanVal = new SNBoolean( boolVal );
+
+			Assert.AreEqual( boolVal, snBooleanVal.Value );
+
+			boolVal = RandomBoolValue;
+			snBooleanVal.Value = boolVal;
+
+			Assert.AreEqual( boolVal, snBooleanVal.Value );
+		}
+
+		[Test]
+		public void TestComparable()
+		{
+			var byteVal1 = RandomBoolValue;
+			var byteVal2 = RandomBoolValue;
+
+			var snBooleanVal1 = RandomBoolValue;
+			var snBooleanVal2 = RandomBoolValue;
+
+			Assert.AreEqual( byteVal1.CompareTo( byteVal2 ), snBooleanVal1.CompareTo( snBooleanVal2 ) );
+			Assert.AreEqual( snBooleanVal1.CompareTo( byteVal2 ), snBooleanVal1.CompareTo( snBooleanVal2 ) );
+		}
+
+		private bool RandomBoolValue
+		{
+			get { return random.Next( int.MinValue, int.MaxValue ) > 0; }
+		}
+	}
+}
