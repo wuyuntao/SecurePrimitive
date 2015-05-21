31d5c2386c2e98a0de5e0f69e0b9f10e448ad016
Wu Yuntao <wyt.brandon@gmail.com>
2015-05-21 19:42:01 +0800

    1, Add implementation of byte and sbyte

---
+++ SecureNumeric/Encryptors/Encryptor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace SecureNumeric.Encryptors
+{
+	abstract class Encryptor
+	{
+		#region Byte
+
+		public abstract byte Encrypt(byte value);
+		public abstract sbyte Encrypt(sbyte value);
+
+		public abstract byte Decrypt(byte value);
+		public abstract sbyte Decrypt(sbyte value);
+
+		#endregion
+
+		#region Int16
+
+		public abstract ushort Encrypt(ushort value);
+		public abstract short Encrypt(short value);
+
+		public abstract ushort Decrypt(ushort value);
+		public abstract short Decrypt(short value);
+
+		#endregion
+
+		#region Int32
+
+		public abstract uint Encrypt(uint value);
+		public abstract int Encrypt(int value);
+
+		public abstract uint Decrypt(uint value);
+		public abstract int Decrypt(int value);
+
+		#endregion
+
+		#region Int64
+
+		public abstract ulong Encrypt(ulong value);
+		public abstract long Encrypt(long value);
+
+		public abstract ulong Decrypt(ulong value);
+		public abstract long Decrypt(long value);
+
+		#endregion
+
+	}
+}
---
+++ SecureNumeric/SecureNumericConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using SecureNumeric.Encryptors;
+using System.Collections.Generic;
+
+namespace SecureNumeric
+{
+	public static class SecureNumericConfiguration
+	{
+		internal static Encryptor GeneratorEncryptor()
+		{
+			return new BitOffsetEncryptor();
+		}
+	}
+}
